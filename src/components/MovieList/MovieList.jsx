302881686fb5019bbeb9195f4608c9c6034e9fcd
NatalyMit <mitenkova91@icloud.com>
2024-03-25 21:41:03 +0100

    corrects

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,12 +3,12 @@ import { Link, useLocation } from 'react-router-dom';
 import MovieItem from '../MovieItem/MovieItem';
 import style from './MovieList.module.css';
 
-const MovieList = ({ filmsList }) => {
+const MovieList = ({ movies }) => {
   const location = useLocation();
   return (
     <div className={style.containerList}>
       <ul className={style.list}>
-        {filmsList.map(({ id, title, poster_path, vote_average }) => (
+        {movies.map(({ id, title, poster_path, vote_average }) => (
           <li key={id} className={style.item}>
             <Link to={`/movies/${id}`} state={{ from: location }}>
               <MovieItem dataFilm={{ title, poster_path, vote_average }} />
